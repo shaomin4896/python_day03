92041500707c81d89844dfc9b46e5d5f8f65794f
shaomin <sam880819@gmail.com>
2020-08-27 09:11:52 +0800

    done

--- gangtu.py
+++ gangtu.py
@@ -9,20 +9,15 @@ class pictor:
     index = 0
     
 pict = pictor()
-pict.sentence = ["aa","bb","ccc","dd"]
+pict.sentence = ["我就爛~~","曾子睿","","124"]
 pict.index = random.randint(0,len(pict.sentence)-1)
-pict.celebrity.hidden = True
 
-pict2 = pictor()
-pict2.celebrity = game.create_sprite("愛因斯坦.jpg") #創造角色
-pict2.sentence = ["1342","3456","456","124"]
-pict2.index = random.randint(0,len(pict.sentence)-1)
 def loop():
     # 顯示文字
-    drawText(pict2.sentence[pict2.index],10,10,"black",80)
+    drawText(pict.sentence[pict.index],10,10,"black",80)
 def rand():
     # 產生隨機數
-    pict2.index = random.randint(0,len(pict2.sentence)-1)
+    pict.index = random.randint(0,len(pict.sentence)-1)
     
 # enter按下偵測
 game.on("keydown","enter", rand)
